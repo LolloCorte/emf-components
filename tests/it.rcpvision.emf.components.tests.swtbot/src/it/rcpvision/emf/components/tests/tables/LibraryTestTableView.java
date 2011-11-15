e6b8217a8c12db80ca910360bf54269339a8843b
Lorenzo Bettini <bettini@dsi.unifi.it>
2011-11-15 21:31:48 +0100

    LibraryTestTableView shows library and books

--- tests/it.rcpvision.emf.components.tests.swtbot/src/it/rcpvision/emf/components/tests/tables/LibraryTestTableView.java
+++ tests/it.rcpvision.emf.components.tests.swtbot/src/it/rcpvision/emf/components/tests/tables/LibraryTestTableView.java
@@ -3,20 +3,26 @@
  */
 package it.rcpvision.emf.components.tests.tables;
 
-import java.util.Collections;
-
 import it.rcpvision.emf.components.resource.ResourceLoader;
 import it.rcpvision.emf.components.views.EmfViewerManager;
 import it.rcpvision.emf.components.views.TableViewerBuilder;
+
+import java.util.Collections;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.examples.extlibrary.EXTLibraryPackage;
 import org.eclipse.emf.examples.extlibrary.Library;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.part.ViewPart;
 
 import com.google.inject.Inject;
@@ -31,8 +37,6 @@ public class LibraryTestTableView extends ViewPart {
 
 	public static final String resourceUri = "platform:/plugin/it.rcpvision.emf.components.tests.swtbot/models/My.extlibrary";
 
-	protected TableViewer tableViewer;
-
 	@Inject
 	protected TableViewerBuilder tableViewerBuilder;
 
@@ -42,30 +46,53 @@ public class LibraryTestTableView extends ViewPart {
 	@Inject
 	protected EmfViewerManager emfViewerManager;
 
+	private Composite composite;
+
 	public LibraryTestTableView() {
 
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		tableViewer = new TableViewer(parent);
-
 		ResourceSet resourceSet = new ResourceSetImpl();
 		Resource resource = resourceLoader.getResource(resourceSet,
 				URI.createURI(resourceUri));
 
 		Library library = (Library) resource.getContents().get(0);
 
+		composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(1, false));
+		
+		Label lblNewLabel = new Label(composite, SWT.NONE);
+		lblNewLabel.setText("Library");
+		
+		TableViewer tableViewer = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION);
 		tableViewerBuilder.buildTableViewer(tableViewer, library.eClass());
 
+		Table table = tableViewer.getTable();
+		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
 		emfViewerManager.initialize(tableViewer,
 				Collections.singleton(library), new ArrayContentProvider(),
+				null);
+		
+		Label lblNewLabel2 = new Label(composite, SWT.NONE);
+		lblNewLabel2.setText("Books");
+		
+		TableViewer tableViewer2 = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION);
+		tableViewerBuilder.buildTableViewer(tableViewer2, EXTLibraryPackage.Literals.BOOK);
+		
+		Table table2 = tableViewer2.getTable();
+		table2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
+		emfViewerManager.initialize(tableViewer2,
+				library.getBooks(), new ArrayContentProvider(),
 				null);
 	}
 
 	@Override
 	public void setFocus() {
-		tableViewer.getTable().setFocus();
+		composite.setFocus();
 	}
 
 }
