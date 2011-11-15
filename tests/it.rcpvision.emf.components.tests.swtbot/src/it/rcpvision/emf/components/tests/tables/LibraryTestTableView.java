502ac89fe4a6f4be86b47ec43e9917b5330cf432
Lorenzo Bettini <bettini@dsi.unifi.it>
2011-11-15 23:35:25 +0100

    extracted method buildTable

--- tests/it.rcpvision.emf.components.tests.swtbot/src/it/rcpvision/emf/components/tests/tables/LibraryTestTableView.java
+++ tests/it.rcpvision.emf.components.tests.swtbot/src/it/rcpvision/emf/components/tests/tables/LibraryTestTableView.java
@@ -8,8 +8,10 @@ import it.rcpvision.emf.components.views.EmfViewerManager;
 import it.rcpvision.emf.components.views.TableViewerBuilder;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -62,32 +64,35 @@ public class LibraryTestTableView extends ViewPart {
 
 		composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new GridLayout(1, false));
-		
+
+
+		buildTable("Library", library, library.eClass());
+		buildTable("Books", library.getBooks(), EXTLibraryPackage.Literals.BOOK);
+		buildTable("Writers", library.getWriters(), EXTLibraryPackage.Literals.WRITER);
+	}
+
+	/**
+	 * @param object
+	 */
+	protected void buildTable(String label, Object object, EClass eClass) {
 		Label lblNewLabel = new Label(composite, SWT.NONE);
-		lblNewLabel.setText("Library");
+		lblNewLabel.setText(label);
 		
-		TableViewer tableViewer = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION);
-		tableViewerBuilder.buildTableViewer(tableViewer, library.eClass());
+		TableViewer tableViewer = new TableViewer(composite, SWT.BORDER
+				| SWT.FULL_SELECTION);
+		tableViewerBuilder.buildTableViewer(tableViewer, eClass);
 
 		Table table = tableViewer.getTable();
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		
-		emfViewerManager.initialize(tableViewer,
-				Collections.singleton(library), new ArrayContentProvider(),
-				null);
-		
-		Label lblNewLabel2 = new Label(composite, SWT.NONE);
-		lblNewLabel2.setText("Books");
-		
-		TableViewer tableViewer2 = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION);
-		tableViewerBuilder.buildTableViewer(tableViewer2, EXTLibraryPackage.Literals.BOOK);
-		
-		Table table2 = tableViewer2.getTable();
-		table2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		
-		emfViewerManager.initialize(tableViewer2,
-				library.getBooks(), new ArrayContentProvider(),
-				null);
+
+		if (object instanceof List<?>) {
+			emfViewerManager.initialize(tableViewer, object,
+					new ArrayContentProvider(), null);
+		} else {
+			emfViewerManager.initialize(tableViewer,
+					Collections.singleton(object), new ArrayContentProvider(),
+					null);
+		}
 	}
 
 	@Override
