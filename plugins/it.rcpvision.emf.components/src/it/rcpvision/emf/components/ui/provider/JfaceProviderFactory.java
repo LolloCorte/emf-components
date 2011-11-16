95bc22191224ee0cb3bd9a5d31e81b5023103f9c
Lorenzo Bettini <bettini@dsi.unifi.it>
2011-11-16 17:52:32 +0100

    create method with no delegate label provider (creates the delegate itself)

--- plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/ui/provider/JfaceProviderFactory.java
+++ plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/ui/provider/JfaceProviderFactory.java
@@ -3,6 +3,8 @@
  */
 package it.rcpvision.emf.components.ui.provider;
 
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 
@@ -18,10 +20,19 @@ public class JfaceProviderFactory {
 	@Inject
 	protected Provider<CompositeLabelProvider> compositeLabelProviderProvider;
 
-	public IBaseLabelProvider createLabelProvider(ILabelProvider delegateLabelProvider) {
+	@Inject
+	protected Provider<ComposedAdapterFactory> composedAdapterFactoryProvider;
+
+	public IBaseLabelProvider createLabelProvider(
+			ILabelProvider delegateLabelProvider) {
 		CompositeLabelProvider compositeLabelProvider = compositeLabelProviderProvider
 				.get();
 		compositeLabelProvider.setDelegateLabelProvider(delegateLabelProvider);
 		return compositeLabelProvider;
+	}
+
+	public IBaseLabelProvider createLabelProvider() {
+		return createLabelProvider(new AdapterFactoryLabelProvider(
+				composedAdapterFactoryProvider.get()));
 	}
 }
