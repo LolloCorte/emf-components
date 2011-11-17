1a20e06c645236f624dc4019d9aed9585057348b
Lorenzo Bettini <bettini@dsi.unifi.it>
2011-11-17 19:12:08 +0100

    the EmfTableView shows the selected EObject

--- plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/views/EmfTableView.java
+++ plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/views/EmfTableView.java
@@ -3,16 +3,20 @@
  */
 package it.rcpvision.emf.components.views;
 
-import java.util.Collections;
-
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.IWorkbenchPart;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 /**
  * A View that visualizes the list of elements of an emf selected resource (it
@@ -21,39 +25,71 @@ import com.google.inject.Inject;
  * @author Lorenzo Bettini
  * 
  */
-public class EmfTableView extends EmfAbstractView {
+public class EmfTableView extends EmfAbstractViewOnSelection {
 
 	@Inject
-	protected TableViewerColumnBuilder tableViewerColumnBuilder;
+	protected TableViewerBuilder tableViewerBuilder;
+
+	@Inject
+	protected Provider<ComposedAdapterFactory> composedAdapterFactoryProvider;
+
+	@Inject
+	protected EmfViewerManager emfViewerManager;
+
+	protected Composite parent;
+
+	protected ScrolledComposite scrolledComposite;
 
 	public EmfTableView() {
 	}
 
 	@Override
-	protected StructuredViewer createViewer(Composite parent) {
-		return new TableViewer(parent);
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+		this.parent = parent;
+		cleanView();
 	}
 
 	@Override
-	protected void performUpdateOnSelection(EObject eObject) {
-		// since we may have to change columns we create a new viewer
-		// as suggested here:
-		// http://www.eclipse.org/forums/index.php/mv/msg/172950/550897/#msg_550897
-		TableViewer tableViewer = (TableViewer) getViewer();
-		Table table = tableViewer.getTable();
-		Composite parent = table.getParent();
-		table.dispose();
-		tableViewer = new TableViewer(parent);
-		setViewer(tableViewer);
+	protected void updateOnSelection(IWorkbenchPart sourcepart,
+			ISelection selection) {
+		cleanView();
 
-		// Create columns
-		tableViewerColumnBuilder.buildTableViewer(tableViewer, eObject.eClass());
-		// no label provider, since we use column label providers
+		Object selectedObject = getFirstSelectedElement(selection);
+		if (selectedObject != null && selectedObject instanceof EObject) {
+			EObject eObject = (EObject) selectedObject;
 
-		emfViewerManager.initialize(tableViewer,
-				Collections.singleton(eObject), new ArrayContentProvider(),
-				null);
-		parent.layout(true, true); // You need to relayout
+			Composite composite = new Composite(scrolledComposite, SWT.BORDER);
+			scrolledComposite.setContent(composite);
+			composite.setLayout(new GridLayout(1, false));
+			composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+			TableViewer tableViewer = new TableViewer(composite, SWT.BORDER
+					| SWT.FULL_SELECTION);
+
+			tableViewerBuilder.buildAndFill(tableViewer, eObject,
+					eObject.eClass());
+
+			Table table = tableViewer.getTable();
+			table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1,
+					1));
+
+			composite.setSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+			parent.layout(true, true);
+		}
+	}
+
+	private void cleanView() {
+		if (scrolledComposite != null)
+			scrolledComposite.dispose();
+
+		scrolledComposite = new ScrolledComposite(parent, SWT.V_SCROLL
+				| SWT.BORDER);
+		scrolledComposite.setExpandHorizontal(true);
+	}
+
+	public void setFocus() {
 	}
 
 }
