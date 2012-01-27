904176da21eabc31e69b95e3a1aee6f5682b9e6f
Emiliano Anichini <emiliano.anichini@rcp-vision.com>
2012-01-27 17:03:55 +0100

    creazione delle extension nel template

--- plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/wizard/masterdetail/TableViewTemplateSection.java
+++ plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/wizard/masterdetail/TableViewTemplateSection.java
@@ -22,13 +22,14 @@ import org.eclipse.pde.internal.ui.IHelpContextIds;
 import org.eclipse.pde.ui.templates.OptionTemplateSection;
 import org.eclipse.pde.ui.templates.PluginReference;
 
-public class TableViewTemplateSection extends OptionTemplateSection  {
-
-	public TableViewTemplateSection(){
+public class TableViewTemplateSection extends OptionTemplateSection {
+	
+	public TableViewTemplateSection() {
 		setPageCount(1);
-	    createOptions();	
+		createOptions();
 	}
 	
+	private static final String PATH_ABSTRACT_DISPATCHER_PREFIX = "it.rcpvision.countervision.ui.cusomer.internal.guice.DemoExecutableExtensionFactory:";
 	private static final String KEY_PACKAGE_NAME = "packageName";
 	private static final String KEY_CLASS_NAME = "className";
 	private static final String KEY_VIEW_NAME = "viewName";
@@ -40,7 +41,7 @@ public class TableViewTemplateSection extends OptionTemplateSection  {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	@Override
 	public URL getTemplateLocation() {
 		// TODO Auto-generated method stub
@@ -49,61 +50,99 @@ public class TableViewTemplateSection extends OptionTemplateSection  {
 	
 	@Override
 	public String[] getNewFiles() {
-		 return new String[] { "icons/" };
+		return new String[] { "icons/" };
 	}
-
+	
 	@Override
 	public String getSectionId() {
 		return "viewtemplate";
 	}
-
+	
 	@Override
 	protected URL getInstallURL() {
 		return EmfComponentsActivator.getDefault().getBundle().getEntry("/");
 	}
-
+	
 	@Override
 	protected ResourceBundle getPluginResourceBundle() {
 		return Platform.getResourceBundle(EmfComponentsActivator.getDefault().getBundle());
 	}
 	
 	public void addPages(Wizard wizard) {
-		 WizardPage page = createPage(0, IHelpContextIds.TEMPLATE_INTRO);
-		 page.setTitle("Table Master Detail Template");
-		 page.setDescription("Creates a Table Master Detail view");
-		 wizard.addPage(page);
-		 markPagesAdded();
+		WizardPage page = createPage(0, IHelpContextIds.TEMPLATE_INTRO);
+		page.setTitle("Table Master Detail Template");
+		page.setDescription("Creates a Table Master Detail view");
+		wizard.addPage(page);
+		markPagesAdded();
 	}
 	
 	private void createOptions() {
-		 addOption(KEY_PACKAGE_NAME, "Package", "test", 0);
-		 addOption(KEY_CLASS_NAME, "Class Name ", "SimpleView", 0);
-		 addOption(KEY_VIEW_NAME, "View Name", "SimpleView", 0);
-		 addOption(KEY_MESSAGE_NAME, "Message", "Hello World", 0);
-		 addOption(KEY_IMP_MESSAGE_NAME, "Important?", false, 0);
+		addOption(KEY_PACKAGE_NAME, "Package", "packageName", 0);
+		addOption(KEY_CLASS_NAME, "Class Name ", "ClassName", 0);
+		addOption(KEY_VIEW_NAME, "View Name", "viewName", 0);
+		addOption(KEY_MESSAGE_NAME, "Message", "message", 0);
+		addOption(KEY_IMP_MESSAGE_NAME, "Important?", false, 0);
 	}
 	
 	protected void updateModel(IProgressMonitor monitor) throws CoreException {
-		 IPluginBase plugin = model.getPluginBase();
-		 IPluginModelFactory factory = model.getPluginFactory();
-		 		 
-		 // org.eclipse.core.runtime.applications
-		 IPluginExtension extension =  createExtension("org.eclipse.ui.views", true);
-		 
-		 IPluginElement element = factory.createElement(extension);
-		 element.setName("view");
-		 element.setAttribute("id", getStringOption(KEY_CLASS_NAME));
-		 element.setAttribute("name", getStringOption(KEY_VIEW_NAME));
-		 element.setAttribute("icon", "icons/sample.gif");
-		 
-		 String fullClassName = 
-		 		 getStringOption(KEY_PACKAGE_NAME) +"."+getStringOption(KEY_CLASS_NAME);
-		 
-		 element.setAttribute("class", fullClassName);
-		 extension.add(element);
-		 
-		 plugin.add(extension);
-	} 
+		IPluginBase plugin = model.getPluginBase();
+		IPluginModelFactory factory = model.getPluginFactory();
+		
+		IPluginExtension extensionView = createExtensionView(factory);
+
+		IPluginExtension extensionMenu = createExtensionMenu(factory);
+		
+		plugin.add(extensionView);
+		plugin.add(extensionMenu);
+	}
+	
+	private IPluginExtension createExtensionMenu(IPluginModelFactory factory)
+			throws CoreException {
+		IPluginExtension extensionMenu = createExtension("org.eclipse.ui.menus", true);
+
+		IPluginElement elementMenu = factory.createElement(extensionMenu);
+		elementMenu.setName("menuContribution");
+		elementMenu.setAttribute("locationURI", "menu:org.eclipse.ui.main.menu");
+		elementMenu.setAttribute("allPopups", "false");
+		
+		IPluginElement elementSubMenu = factory.createElement(elementMenu);
+		elementSubMenu.setName("command");
+		elementSubMenu.setAttribute("commandId", "org.eclipse.ui.views.showView");
+		elementSubMenu.setAttribute("label", getStringOption(KEY_VIEW_NAME));
+		elementSubMenu.setAttribute("tooltip", "Open the Master Detail");
+		elementSubMenu.setAttribute("style", "push");
+		
+		IPluginElement elementSubMenuParameter = factory.createElement(elementSubMenu);
+		elementSubMenuParameter.setName("parameter");
+		elementSubMenuParameter.setAttribute("name", "org.eclipse.ui.views.showView.viewId");
+		String viewId = getStringOption(KEY_PACKAGE_NAME) + "." + getStringOption(KEY_CLASS_NAME);
+		elementSubMenuParameter.setAttribute("value", viewId);
+		
+		extensionMenu.add(elementMenu);
+		elementMenu.add(elementSubMenu);
+		elementSubMenu.add(elementSubMenuParameter);
+
+		return extensionMenu;
+	}
+	
+	private IPluginExtension createExtensionView(IPluginModelFactory factory)
+			throws CoreException {
+		IPluginExtension extensionView = createExtension("org.eclipse.ui.views", true);
+
+		IPluginElement elementView = factory.createElement(extensionView);
+		elementView.setName("view");
+		String viewId = getStringOption(KEY_PACKAGE_NAME) + "." + getStringOption(KEY_CLASS_NAME);
+		elementView.setAttribute("id", viewId);
+		elementView.setAttribute("name", getStringOption(KEY_VIEW_NAME));
+
+		String fullClassName = PATH_ABSTRACT_DISPATCHER_PREFIX + viewId;
+		elementView.setAttribute("class", fullClassName);
+		elementView.setAttribute("icon", "icons/sample.gif");
+		
+		extensionView.add(elementView);
+
+		return extensionView;
+	}
 	
 	@Override
 	public IPluginReference[] getDependencies(String schemaVersion) {
