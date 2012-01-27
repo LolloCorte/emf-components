85c33fbebcdea22b1b0f374e949f5a454f48ea41
Francesco Guidieri <francesco.guidieri@rcp-vision.com>
2012-01-27 08:38:25 +0100

    implementazione iniziale wizard

---
+++ plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/wizard/masterdetail/TableViewTemplateSection.java
@@ -0,0 +1,102 @@
+package it.rcpvision.emf.components.wizard.masterdetail;
+
+import it.rcpvision.emf.components.EmfComponentsActivator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.pde.core.plugin.IPluginBase;
+import org.eclipse.pde.core.plugin.IPluginElement;
+import org.eclipse.pde.core.plugin.IPluginExtension;
+import org.eclipse.pde.core.plugin.IPluginModelFactory;
+import org.eclipse.pde.internal.ui.IHelpContextIds;
+import org.eclipse.pde.ui.templates.OptionTemplateSection;
+
+public class TableViewTemplateSection extends OptionTemplateSection  {
+
+	public TableViewTemplateSection(){
+		setPageCount(1);
+	    createOptions();	
+	}
+	
+	private static final String KEY_CLASS_NAME = "className";
+	private static final String KEY_VIEW_NAME = "viewName";
+	private static final String KEY_MESSAGE_NAME = "message";
+	private static final String KEY_IMP_MESSAGE_NAME = "importantMessage";
+	
+	@Override
+	public String getUsedExtensionPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL getTemplateLocation() {
+		// TODO Auto-generated method stub
+		return super.getTemplateLocation();
+	}
+	
+	@Override
+	public String[] getNewFiles() {
+		 return new String[] { "icons/" };
+	}
+
+	@Override
+	public String getSectionId() {
+		return "viewtemplate";
+	}
+
+	@Override
+	protected URL getInstallURL() {
+		return EmfComponentsActivator.getDefault().getBundle().getEntry("/");
+	}
+
+	@Override
+	protected ResourceBundle getPluginResourceBundle() {
+		return Platform.getResourceBundle(EmfComponentsActivator.getDefault().getBundle());
+	}
+	
+	public void addPages(Wizard wizard) {
+		 WizardPage page = createPage(0, IHelpContextIds.TEMPLATE_INTRO);
+		 page.setTitle("Table Master Detail Template");
+		 page.setDescription("Creates a Table Master Detail view");
+		 wizard.addPage(page);
+		 markPagesAdded();
+	}
+	
+	private void createOptions() {
+		 addOption(KEY_PACKAGE_NAME, "Package", "test", 0);
+		 addOption(KEY_CLASS_NAME, "Class Name ", "SimpleView", 0);
+		 addOption(KEY_VIEW_NAME, "View Name", "SimpleView", 0);
+		 addOption(KEY_MESSAGE_NAME, "Message", "Hello World", 0);
+		 addOption(KEY_IMP_MESSAGE_NAME, "Important?", false, 0);
+	}
+	
+	protected void updateModel(IProgressMonitor monitor) throws CoreException {
+		 IPluginBase plugin = model.getPluginBase();
+		 IPluginModelFactory factory = model.getPluginFactory();
+		 		 
+		 // org.eclipse.core.runtime.applications
+		 IPluginExtension extension =  createExtension("org.eclipse.ui.views", true);
+		 
+		 IPluginElement element = factory.createElement(extension);
+		 element.setName("view");
+		 element.setAttribute("id", getStringOption(KEY_CLASS_NAME));
+		 element.setAttribute("name", getStringOption(KEY_VIEW_NAME));
+		 element.setAttribute("icon", "icons/sample.gif");
+		 
+		 String fullClassName = 
+		 		 getStringOption(KEY_PACKAGE_NAME) +"."+getStringOption(KEY_CLASS_NAME);
+		 
+		 element.setAttribute("class", fullClassName);
+		 extension.add(element);
+		 
+		 plugin.add(extension);
+} 
+
+}
---
+++ plugins/it.rcpvision.emf.components/templates_3.4/viewtemplate/java/$className$.java
@@ -0,0 +1,23 @@
+package $packageName$;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.part.ViewPart;
+
+public class $className$ extends ViewPart {
+		 
+		 public void createPartControl(Composite parent) {
+		 		 Label label = new Label(parent, SWT.CENTER);
+		 		 
+%if importantMessage
+		 		 String message = new String("$message$!!!");
+%else
+		 		 String message = new String("$message$");
+%endif
+		 		 label.setText(message);
+		 }
+		 
+		 public void setFocus() {}
+		 
+}
