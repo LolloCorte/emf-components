3abb1d5477bd2f8095c3618c7fd6ac16a688227f
Emiliano Anichini <emiliano.anichini@rcp-vision.com>
2012-01-30 11:44:38 +0100

    Modifica con Francesco a CDOSessionManager

--- plugins/it.rcpvision.emf.components.cdo/src/it/rcpvision/emf/components/cdo/CDOSessionManager.java
+++ plugins/it.rcpvision.emf.components.cdo/src/it/rcpvision/emf/components/cdo/CDOSessionManager.java
@@ -1,11 +1,22 @@
 package it.rcpvision.emf.components.cdo;
 
 import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.databinding.IEMFListProperty;
 import org.eclipse.emf.ecore.EObject;
 
 public class CDOSessionManager {
 
 	public CDOSession getSession(EObject obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public EObject getContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public IEMFListProperty getListProperty() {
 		// TODO Auto-generated method stub
 		return null;
 	}
