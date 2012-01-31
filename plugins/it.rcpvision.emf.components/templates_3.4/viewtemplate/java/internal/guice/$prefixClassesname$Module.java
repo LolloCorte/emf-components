c2f0404e6ebcbd1676328a4f01bdb888c997b8e4
Lorenzo Murrocu <lorenzo.murrocu@rcp-vision.com>
2012-01-31 18:14:49 +0100

    Template BindingFactory con date picker di esempio

--- plugins/it.rcpvision.emf.components/templates_3.4/viewtemplate/java/internal/guice/$prefixClassesname$Module.java
+++ plugins/it.rcpvision.emf.components/templates_3.4/viewtemplate/java/internal/guice/$prefixClassesname$Module.java
@@ -4,6 +4,7 @@ package $packageName$.internal.guice;
 import $packageName$.$prefixClassesname$ViewConfigurator;
 import it.rcpvision.emf.components.cdo.CDOEmfComponentsModule;
 import it.rcpvision.emf.components.cdo.ViewConfigurator;
+import it.rcpvision.emf.components.ui.binding.EmfSwtBindingFactory;
 import it.rcpvision.emf.components.ui.provider.FeatureLabelProvider;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -21,6 +22,11 @@ public class $prefixClassesname$Module extends CDOEmfComponentsModule {
 	@Override
 	public Class<? extends FeatureLabelProvider> bindFeatureLabelProvider() {
 		return $prefixClassesname$FeatureLabelProvider.class;
+	}
+	
+	@Override
+	public Class<? extends EmfSwtBindingFactory> bindEmfSwtBindingFactory() {
+		return $prefixClassesname$BindingFactory.class;
 	}
 	
 }
