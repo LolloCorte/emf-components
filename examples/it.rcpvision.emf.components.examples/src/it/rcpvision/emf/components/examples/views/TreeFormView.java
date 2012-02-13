912ccae084f360cbf3a195805551bc03573fb7f9
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-02-13 18:06:06 +0100

    extracted a TreeFormComposite

--- examples/it.rcpvision.emf.components.examples/src/it/rcpvision/emf/components/examples/views/TreeFormView.java
+++ examples/it.rcpvision.emf.components.examples/src/it/rcpvision/emf/components/examples/views/TreeFormView.java
@@ -1,48 +1,18 @@
 package it.rcpvision.emf.components.examples.views;
 
+import it.rcpvision.emf.components.examples.composite.TreeFormComposite;
 import it.rcpvision.emf.components.views.EmfAbstractViewOnSelection;
 import it.rcpvision.emf.components.views.EmfDetailsFactory;
 import it.rcpvision.emf.components.views.EmfViewerManager;
-import it.rcpvision.emf.components.views.FormDetailComposite;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.StructuredViewer;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.part.PageBook;
 
 import com.google.inject.Inject;
 
 public class TreeFormView extends EmfAbstractViewOnSelection {
-
-	protected class SelectionChangedListener implements
-			ISelectionChangedListener {
-		@Override
-		public void selectionChanged(SelectionChangedEvent event) {
-			EObject object = getFirstSelectedEObject(event.getSelection());
-			if (object != null) {
-				if (detailForm != null)
-					detailForm.dispose();
-				
-				detailForm = emfDetailsFactory
-						.createFormDetailComposite(detail, SWT.BORDER);
-				detailForm.init(object);
-				detail.layout(true);
-			}
-		}
-	}
 
 	@Inject
 	protected EmfViewerManager emfViewerManager;
@@ -50,58 +20,25 @@ public class TreeFormView extends EmfAbstractViewOnSelection {
 	@Inject
 	protected EmfDetailsFactory emfDetailsFactory;
 
-	private StructuredViewer treeViewer;
-	private PageBook pagebook;
-	private Composite detail;
-
-	protected FormDetailComposite detailForm;
+	protected TreeFormComposite treeFormComposite;
 
 	@Override
 	public void createPartControl(Composite parent) {
 		super.createPartControl(parent);
-		
-		SashForm sashForm = new SashForm(parent, SWT.VERTICAL);
-		GridLayoutFactory.fillDefaults().applyTo(sashForm);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(sashForm);
-		
-		pagebook = new PageBook(sashForm, SWT.BORDER);
-		detail = new Composite(sashForm, SWT.BORDER);
-		detail.setLayout(new FillLayout());
-		treeViewer = createViewer(pagebook);
-		treeViewer.addSelectionChangedListener(new SelectionChangedListener());
+
+		treeFormComposite = new TreeFormComposite(parent, SWT.BORDER,
+				emfViewerManager, emfDetailsFactory);
 	}
 
 	@Override
 	protected void updateOnSelection(IWorkbenchPart sourcepart,
 			ISelection selection) {
-		Object element = getFirstSelectedElement(selection);
-
-		if (element != null) {
-			if (element instanceof IResource) {
-				IResource resource = (IResource) element;
-				URI uri = URI.createPlatformResourceURI(resource.getFullPath()
-						.toString(), true);
-				emfViewerManager.initialize(treeViewer, uri);
-			} else if (element instanceof Resource) {
-				Resource resource = (Resource) element;
-				emfViewerManager.initialize(treeViewer, resource);
-			} else if (element instanceof EObject) {
-				EObject eObject = (EObject) element;
-				emfViewerManager.initialize(treeViewer, eObject);
-			}
-			pagebook.showPage(treeViewer.getControl());
-		}
-
-	}
-
-	protected StructuredViewer createViewer(Composite parent) {
-		return new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		treeFormComposite.update(getFirstSelectedElement(selection));
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-
+		treeFormComposite.setFocus();
 	}
 
 }
