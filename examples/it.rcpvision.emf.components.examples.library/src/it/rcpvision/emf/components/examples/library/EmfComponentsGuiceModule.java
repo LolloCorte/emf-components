8585e9d6521cfee45a48c8d7c0f74c594635957c
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-03-05 10:47:58 +0100

    added an example based on library

---
+++ examples/it.rcpvision.emf.components.examples.library/src/it/rcpvision/emf/components/examples/library/EmfComponentsGuiceModule.java
@@ -0,0 +1,13 @@
+package it.rcpvision.emf.components.examples.library;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import it.rcpvision.emf.components.service.EmfComponentsGenericModule;
+
+public class EmfComponentsGuiceModule extends EmfComponentsGenericModule {
+
+	public EmfComponentsGuiceModule(AbstractUIPlugin plugin) {
+		super(plugin);
+	}
+
+}
---
+++ examples/it.rcpvision.emf.components.examples.library/src/it/rcpvision/emf/components/examples/library/ExecutableExtensionFactory.java
@@ -0,0 +1,21 @@
+package it.rcpvision.emf.components.examples.library;
+
+import org.osgi.framework.Bundle;
+
+import it.rcpvision.emf.components.EmfComponentsExecutableExtensionFactory;
+import it.rcpvision.emf.components.service.EmfComponentsGenericModule;
+
+public class ExecutableExtensionFactory extends
+		EmfComponentsExecutableExtensionFactory {
+
+	@Override
+	protected Bundle getBundle() {
+		return Activator.getDefault().getBundle();
+	}
+
+	@Override
+	protected EmfComponentsGenericModule getModule() {
+		return new EmfComponentsGuiceModule(Activator.getDefault());
+	}
+
+}
