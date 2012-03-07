3b986705fa357fc22e817437e49e6fc6a2d01be0
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-03-07 17:47:08 +0100

    fixed swtbot test after refactoring

--- tests/it.rcpvision.emf.components.tests.swtbot/src/it/rcpvision/emf/components/tests/EmfComponentsAbstractTests.java
+++ tests/it.rcpvision.emf.components.tests.swtbot/src/it/rcpvision/emf/components/tests/EmfComponentsAbstractTests.java
@@ -115,7 +115,7 @@ public class EmfComponentsAbstractTests {
 
 	protected static final String LIBRARY_EMF_VIEW_CUSTOM_LABEL = "LibraryEmfView Custom Label";
 
-	protected static final String EMF_DETAIL_VIEW = "Emf Detail View";
+	protected static final String EMF_DETAIL_VIEW = "Emf Form View";
 	
 	protected static final String EMF_TREE_FORM_DETAIL_VIEW = "Emf Tree Form View";
 
