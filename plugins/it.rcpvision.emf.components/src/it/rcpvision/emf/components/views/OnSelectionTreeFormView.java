106d52e1e7b9b566bee84a241b3df888cee08ca1
Francesco Guidieri <francesco.guidieri@gmail.com>
2012-03-08 15:46:01 +0100

    renamed TreeFormView to OnSelectionTreeFormView

--- plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/views/OnSelectionTreeFormView.java
+++ plugins/it.rcpvision.emf.components/src/it/rcpvision/emf/components/views/OnSelectionTreeFormView.java
@@ -10,7 +10,7 @@ import org.eclipse.ui.IWorkbenchPart;
 
 import com.google.inject.Inject;
 
-public class TreeFormView extends OnSelectionAbstractView {
+public class OnSelectionTreeFormView extends OnSelectionAbstractView {
 
 	@Inject
 	protected EmfCompositeFactory emfCompositeFactory;
