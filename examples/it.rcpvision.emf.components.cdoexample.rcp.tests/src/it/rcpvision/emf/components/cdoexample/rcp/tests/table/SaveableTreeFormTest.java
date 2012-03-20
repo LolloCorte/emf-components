ba7518c3d37a9ede4f1d0c4b5883755fce0ecdf8
Francesca <francesca.mastinu@rcp-vision.com>
2012-03-20 17:19:01 +0100

    Test per  SaveableTreeForm - prima versione

--- examples/it.rcpvision.emf.components.cdoexample.rcp.tests/src/it/rcpvision/emf/components/cdoexample/rcp/tests/table/SaveableTreeFormTest.java
+++ examples/it.rcpvision.emf.components.cdoexample.rcp.tests/src/it/rcpvision/emf/components/cdoexample/rcp/tests/table/SaveableTreeFormTest.java
@@ -1,29 +1,97 @@
 package it.rcpvision.emf.components.cdoexample.rcp.tests.table;
 
-import static org.junit.Assert.assertTrue;
+import java.util.List;
 
+import org.eclipse.jface.bindings.keys.ParseException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTableItem;
+import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
-public class SaveableTreeFormTest extends CdoExampleUiTable{
+public class SaveableTreeFormTest extends CdoExampleUiTable {
 
 	private static final String LIBRARY = "Library";
 	private static final String SAVEABLE_TREEFORM_VIEW_TITLE = "Saveable Resource CDO Tree Form View";
-	private static final String BOOK_CIAO = "Book Ciao";
+//	private static final String BOOK_CIAO = "Book Book Ciao";
+	private static final String BOOK_NOME_LIBRO = "Book";
 
+	
+	
 	@Test
 	public void testExpandLibrary() {
 		bot.viewByTitle(SAVEABLE_TREEFORM_VIEW_TITLE).setFocus();
 		bot.tree().expandNode(LIBRARY);
 	}
-	
+
+
 	@Test
 	public void testSelectBook() {
 		bot.viewByTitle(SAVEABLE_TREEFORM_VIEW_TITLE).setFocus();
-		bot.tree().select(BOOK_CIAO);
-		bot.sleep(2000);
+		SWTBotTreeItem root = bot.tree().expandNode(LIBRARY);
+		root.expand();
+		root.getNode("Book Book").select();
 	}
+
+	@Test
+	public void insertAndSaveDetailsBook() throws WidgetNotFoundException,
+			ParseException {
+		bot.textWithLabel("title").setText("Ciccio Pasticcio");
+		openDisplayNameAuthorDialog();
+		bot.textWithLabel("pages").setText("458");
+		saveModifications();
+	}
+
+	@Test 
+	public void resetModifications() throws WidgetNotFoundException, ParseException{
+		bot.viewByTitle(SAVEABLE_TREEFORM_VIEW_TITLE).setFocus();
+		SWTBotTreeItem root = bot.tree().expandNode(LIBRARY);
+		root.expand();
+		root.getNode("Book Ciccio Pasticcio").select();
+		bot.textWithLabel("title").setText(BOOK_NOME_LIBRO);
+		saveModifications();
+	}
+	
+	public void saveModifications() throws WidgetNotFoundException,
+			ParseException {
+		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
+		System.out.println("Nome activeShell:" + bot.activeShell().getText());
+		char s = 's';
+		bot.activeShell().pressShortcut(SWT.CTRL, s);
+	}
+
+	private void openDisplayNameAuthorDialog() {
+		bot.button("...").click();
+		SWTBotShell displayNameShell = bot.activeShell();
+		displayNameShell.bot().button("Cancel").click();
+	}
+
+	private void getBook(SWTBotTreeItem root, String bookName) {
+		root.expand().getNode(bookName).select();
+	}
+
+	@Test
+	public void verifyContentTree(){
+		bot.viewByTitle(SAVEABLE_TREEFORM_VIEW_TITLE).setFocus();
+		SWTBotTreeItem root = bot.tree().expandNode(LIBRARY);
+		root.select();
+		List<String> contentTree = root.getNodes();
+		for (String swtBotTreeItem : contentTree) {
+			System.out.println("Elementi dell'albero: " + swtBotTreeItem);
+		}
+		
+	}
+	
+//	@Test
+	public void testCreateBook() {
+		bot.viewByTitle(SAVEABLE_TREEFORM_VIEW_TITLE).setFocus();
+		SWTBotTreeItem root = bot.tree().expandNode(LIBRARY);
+		root.select();
+
+	}
+
 }
