05bf4db15ae54d5cacf60d22afbf4230591940f2
Francesco Guidieri <francesco.guidieri@gmail.com>
2012-03-27 16:30:59 +0200

    Saveable Resource Table example

--- examples/it.rcpvision.emf.components.examples/src/it/rcpvision/emf/components/examples/EmfComponentsExamplesModule.java
+++ examples/it.rcpvision.emf.components.examples/src/it/rcpvision/emf/components/examples/EmfComponentsExamplesModule.java
@@ -1,6 +1,8 @@
 package it.rcpvision.emf.components.examples;
 
 import it.rcpvision.emf.components.EmfComponentsGenericModule;
+import it.rcpvision.emf.components.views.TableViewerColumnBuilder;
+import it.rcpvision.emf.components.views.TableViewerEditableColumnBuilder;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
@@ -9,5 +11,11 @@ public class EmfComponentsExamplesModule extends EmfComponentsGenericModule {
 	public EmfComponentsExamplesModule(AbstractUIPlugin plugin) {
 		super(plugin);
 	}
+	
+	@Override
+	public Class<? extends TableViewerColumnBuilder> bindTableViewerColumnBuilder() {
+		return TableViewerEditableColumnBuilder.class;
+	}
+
 
 }
---
+++ examples/it.rcpvision.emf.components.examples/src/it/rcpvision/emf/components/examples/views/SaveableResourceTableView.java
@@ -0,0 +1,30 @@
+package it.rcpvision.emf.components.examples.views;
+
+import it.rcpvision.emf.components.examples.library.EXTLibraryPackage;
+import it.rcpvision.emf.components.examples.library.Library;
+import it.rcpvision.emf.components.views.AbstractSaveableResourceTableView;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.resource.Resource;
+
+public class SaveableResourceTableView extends
+		AbstractSaveableResourceTableView {
+
+	@Override
+	protected Object getContents(Resource resource) {
+		Library library=(Library) resource.getContents().get(0);
+		return library.getBooks();
+	}
+
+	@Override
+	protected EClass getEClass() {
+		return EXTLibraryPackage.Literals.BOOK;
+	}
+
+	@Override
+	protected URI createResourceURI() {
+		return URI.createPlatformResourceURI("/library/Library.xmi", true);
+	}
+
+}
