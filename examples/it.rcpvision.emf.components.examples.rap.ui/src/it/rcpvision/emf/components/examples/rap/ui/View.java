45433e41dfd450c5f1edaa32dd736288974b6cb6
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-07-30 13:57:23 +0200

    RAP example uses TreeFormComposite
    Change-Id: I4967b3dbbebf513ac8e35da9f224c4450710fc1d

--- examples/it.rcpvision.emf.components.examples.rap.ui/src/it/rcpvision/emf/components/examples/rap/ui/View.java
+++ examples/it.rcpvision.emf.components.examples.rap.ui/src/it/rcpvision/emf/components/examples/rap/ui/View.java
@@ -1,13 +1,11 @@
 package it.rcpvision.emf.components.examples.rap.ui;
 
+import it.rcpvision.emf.components.factories.TreeFormFactory;
 import it.rcpvision.emf.components.resource.EmptyResourceInitializer;
-import it.rcpvision.emf.components.ui.provider.CompositeLabelProvider;
-import it.rcpvision.emf.components.viewers.ViewerInitializer;
+import it.rcpvision.emf.components.widgets.TreeFormComposite;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
-import org.eclipse.jface.viewers.StructuredViewer;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
@@ -17,37 +15,30 @@ import com.google.inject.Inject;
 public class View extends ViewPart {
 	public static final String ID = "it.rcpvision.emf.components.examples.rap.ui.view";
 
-	private StructuredViewer viewer;
+	@Inject private EmptyResourceInitializer emptyResourceInitializer;
 	
-	@Inject protected CompositeLabelProvider labelProvider;
+	@Inject private TreeFormFactory treeFormFactory;
 	
-	@Inject protected ViewerInitializer viewerInitializer;
-	
-	@Inject protected EmptyResourceInitializer emptyResourceInitializer;
+	protected TreeFormComposite treeFormComposite;
 
 	/**
 	 * This is a callback that will allow us to create the viewer and initialize
 	 * it.
 	 */
 	public void createPartControl(Composite parent) {
-		int style = SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL;
-		viewer = new TreeViewer(parent, style);
+		treeFormComposite = treeFormFactory.createTreeFormMasterDetailComposite(parent,
+				SWT.BORDER);
 		
 		Resource resource = new ResourceImpl();
 		emptyResourceInitializer.initialize(resource);
 		
-		viewerInitializer.initialize(viewer, resource);
-		
-//		viewer.setContentProvider(new ViewContentProvider());
-//		labelProvider.setDelegateLabelProvider(new ViewLabelProvider());
-//		viewer.setLabelProvider(labelProvider);
-//		viewer.setInput(getViewSite());
+		treeFormComposite.update(resource);
 	}
 
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
-		viewer.getControl().setFocus();
+		treeFormComposite.setFocus();
 	}
 }
