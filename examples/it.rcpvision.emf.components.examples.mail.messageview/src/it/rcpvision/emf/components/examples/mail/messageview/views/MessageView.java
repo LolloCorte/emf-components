3c881a865c3772c6473e5f98221bbf37ddf93a0a
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-10-19 22:26:52 +0200

    created mail MessageView
    Change-Id: Ie4ddeacffec9a8e1c1cd9de64bef1565bcac58b0

---
+++ examples/it.rcpvision.emf.components.examples.mail.messageview/src/it/rcpvision/emf/components/examples/mail/messageview/views/MessageView.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package it.rcpvision.emf.components.examples.mail.messageview.views;
+
+import it.rcpvision.emf.components.views.OnSelectionFormView;
+
+/**
+ * @author Lorenzo Bettini
+ *
+ */
+public class MessageView extends OnSelectionFormView {
+
+}
