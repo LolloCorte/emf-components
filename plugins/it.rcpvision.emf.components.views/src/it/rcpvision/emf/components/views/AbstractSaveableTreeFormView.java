d85c3012021b1f368ec4e224c06a88f99986aa2e
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-11-17 11:45:05 +0100

    set selection on the new child is the default for saveable views
    Change-Id: Ifd91f1fa6a9e0ded44eb1eaf62aeabe5c2828b80

--- plugins/it.rcpvision.emf.components.views/src/it/rcpvision/emf/components/views/AbstractSaveableTreeFormView.java
+++ plugins/it.rcpvision.emf.components.views/src/it/rcpvision/emf/components/views/AbstractSaveableTreeFormView.java
@@ -3,12 +3,7 @@ package it.rcpvision.emf.components.views;
 import it.rcpvision.emf.components.factories.TreeFormFactory;
 import it.rcpvision.emf.components.widgets.TreeFormComposite;
 
-import java.util.Collection;
-
-import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.edit.command.CreateChildCommand;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -37,37 +32,6 @@ public abstract class AbstractSaveableTreeFormView extends AbstractSaveableView
 		treeFormComposite.update(getContents(getResource()));
 
 		addContextMenu(treeFormComposite.getViewer());
-	}
-
-	@Override
-	protected void customizePostCommandStackChanged(Command mostRecentCommand) {
-		if (mostRecentCommand != null
-				&& mostRecentCommand instanceof CreateChildCommand) {
-			setSelectionToViewer(mostRecentCommand.getAffectedObjects());
-		}
-	}
-
-	public void setSelectionToViewer(Collection<?> collection) {
-		final Collection<?> theSelection = collection;
-		// Make sure it's okay.
-		//
-		if (theSelection != null && !theSelection.isEmpty()) {
-			Runnable runnable = new Runnable() {
-				@Override
-				public void run() {
-					// Try to select the items in the current content viewer of
-					// the editor.
-					//
-					if (treeFormComposite.getViewer() != null) {
-						treeFormComposite.getViewer()
-								.setSelection(
-										new StructuredSelection(
-												theSelection.toArray()), true);
-					}
-				}
-			};
-			getSite().getShell().getDisplay().asyncExec(runnable);
-		}
 	}
 
 	@Override
--- plugins/it.rcpvision.emf.components.views/src/it/rcpvision/emf/components/views/AbstractSaveableView.java
+++ plugins/it.rcpvision.emf.components.views/src/it/rcpvision/emf/components/views/AbstractSaveableView.java
@@ -8,6 +8,7 @@ import it.rcpvision.emf.components.util.EmfCommandsUtil;
 import it.rcpvision.emf.components.viewers.ViewerInitializer;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.EventObject;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -17,11 +18,13 @@ import org.eclipse.emf.common.command.CommandStackListener;
 import org.eclipse.emf.common.ui.viewer.IViewerProvider;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.edit.command.CreateChildCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.ISaveablePart;
@@ -75,7 +78,6 @@ public abstract class AbstractSaveableView extends ViewPart implements
 								.asyncExec(new Runnable() {
 									public void run() {
 										// Try to select the affected objects.
-										//
 										Command mostRecentCommand = EmfCommandsUtil
 												.mostRecentCommand(event);
 
@@ -85,7 +87,7 @@ public abstract class AbstractSaveableView extends ViewPart implements
 											mostRecentCommandAffectsResource(mostRecentCommand);
 										}
 
-										customizePostCommandStackChanged(mostRecentCommand);
+										postCommandStackChanged(mostRecentCommand);
 									}
 
 								});
@@ -93,8 +95,42 @@ public abstract class AbstractSaveableView extends ViewPart implements
 				});
 	}
 
-	protected void customizePostCommandStackChanged(Command mostRecentCommand) {
-		// do nothing
+	/**
+	 * This is called after mostRecentCommandAffectsResource, so that you can
+	 * perform additional custom actions.
+	 * 
+	 * The default implementation is to select in the viewer the possible new
+	 * added child; this will also make context menu work seamlessly (if the
+	 * selection stays in the parent element, then it will not obviously change
+	 * and the context menu actions will not be recreated and they will be
+	 * stale).
+	 * 
+	 * @param mostRecentCommand
+	 */
+	protected void postCommandStackChanged(Command mostRecentCommand) {
+		if (mostRecentCommand != null
+				&& mostRecentCommand instanceof CreateChildCommand) {
+			setSelectionToViewer(mostRecentCommand.getAffectedObjects());
+		}
+	}
+
+	protected void setSelectionToViewer(Collection<?> collection) {
+		final Collection<?> theSelection = collection;
+		if (theSelection != null && !theSelection.isEmpty()) {
+			Runnable runnable = new Runnable() {
+				@Override
+				public void run() {
+					// Try to select the items in the current viewer.
+					if (getViewer() != null) {
+						getViewer()
+								.setSelection(
+										new StructuredSelection(
+												theSelection.toArray()), true);
+					}
+				}
+			};
+			getSite().getShell().getDisplay().asyncExec(runnable);
+		}
 	}
 
 	/**
